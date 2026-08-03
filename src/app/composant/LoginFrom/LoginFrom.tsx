b90c659f24ca77f1c8436c2d77798222c2bb0b90
BLASCOJ24 <j.blasco69@gmail.com>
2026-08-03 03:41:44 +0200

    refactor: reorder username and password variable assignments for clarity

--- src/app/composant/LoginFrom/LoginFrom.tsx
+++ src/app/composant/LoginFrom/LoginFrom.tsx
@@ -14,11 +14,12 @@ export default function LoginFrom() {
 		e.preventDefault();
 
 		const form = new FormData(e.currentTarget);
-		//const userName = form.get("username");
-		//const password = form.get("password");
+		const userName = form.get("username");
+		const password = form.get("password");
 
-		const password = "password789";
-		const userName = "emmaleroy";
+		//for test
+		//const password = "password789";
+		//const userName = "emmaleroy";
 
 		try {
 			const response = await fetch("http://localhost:8000/api/login", {
